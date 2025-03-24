1f98f1b94b8f02931f34393e3f4b5b599f7a2326
Alex Sefer <alexandrit98226@gmail.com>
2025-03-24 22:23:09 +0200

    returned base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-deploy-cicd",
   server: {
     open: true,
     port: 3000,
